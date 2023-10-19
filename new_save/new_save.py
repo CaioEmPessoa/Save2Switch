1265ee521e4bf3ad7e8c3338705a398c8bb40965
CaioEmPessoa <caiopolonioabruno@gmail.com>
2023-10-19 17:23:13 -0300

    Changed new view window to add icons path

--- new_save/new_save.py
+++ new_save/new_save.py
@@ -4,49 +4,39 @@ from tkinter import filedialog
 
 class NewSave():
     def create_newsave_window(self, main, new_save_view):
-
-
         self.main = main
 
         self.new_view = new_save_view.NewSaveView(self)
-        #self.new_view.grab_set()
-        self.new_view.mainloop()
-
+        self.new_view.grab_set()
 
     def send(self):
 
         self.name = self.new_view.name_entry.get()
         self.pc_path = self.new_view.pc_path_entry.get()
         self.switch_path = self.new_view.switch_path_entry.get()
+        self.icon_path = self.new_view.self.image_path_entry.get()
 
         if self.pc_path == "" or self.name == "" or self.switch_path == "":
-            print("FILL ALL THE FIELDS")
-            self.new_view.send_button.configure(fg_color="Red", text="Caminho Vazio")
+            self.new_view.wanring.configure(fg_color="#b60000", text="FILL IN ALL THE * ENTRIES")
 
         else:
             current_app_dic = {
                 self.name: {
                     "name": f"{self.name}",
                     "pc_path": f"\"{self.pc_path}\"",
-                    "switch_path": f"{self.switch_path}"
+                    "switch_path": f"{self.switch_path}",
+                    "icon_path": f"{self.icon_path}"
                 }
             }
             
-            #self.main.write_data(current_app_dic)
-            
-            self.main.call_window("close")
+            self.main.write_data(current_app_dic)
+
             self.main.call_window("restart")
 
-    def search_window(self, path_icon):
-        if path_icon == "pc":
-            self.new_view.pc_path_entry.delete(0, "end")
-            self.pc_path = filedialog.askopenfilename()
-            self.new_view.pc_path_entry.insert(0, self.pc_path)
-
-        elif path_icon == "switch":
-            self.new_view.switch_path_entry.delete(0, "end")
-            self.switch_path = filedialog.askopenfilename()
-            self.new_view.switch_path_entry.insert(0, self.switch_path)
+    def search_window(self, entry):
+            entry.delete(0, "end")
+            path = filedialog.askopenfilename()
+            entry.insert(0, path)
 
     def site_app(self):
 
@@ -66,6 +56,3 @@ class NewSave():
 
             self.browser_entry.grid_forget()
             self.browser_label.grid_forget()
-
-
-s = NewSave()
--- new_save/new_save_view.py
+++ new_save/new_save_view.py
@@ -4,43 +4,54 @@ from PIL import Image
 import shutil
 
 class NewSaveView(ctk.CTkToplevel):
-    def __init__(self, newsave_src):
+    def __init__(self, new_save):
         super().__init__()
 
         # Buttons, Labels and Entrys
         # LABELS
-        self.name_label = ctk.CTkLabel(master=self, justify="left", text="Choose the name of the game:")
+        self.name_label = ctk.CTkLabel(master=self, justify="left", text="*Choose the name of the game:")
         self.name_label.grid(row=0, column=0, padx=10, columnspan=3, sticky="W")
 
-        self.pc_path_label = ctk.CTkLabel(master=self, text="Select the path to the PC save foulder:")
+        self.pc_path_label = ctk.CTkLabel(master=self, text="*Select the path to the PC save foulder:")
         self.pc_path_label.grid(row=3, column=0, padx=10, columnspan=2, sticky="W")
 
-        self.switch_path_label = ctk.CTkLabel(master=self, text="Select the path to the Switch save foulder:")
+        self.switch_path_label = ctk.CTkLabel(master=self, text="*Select the path to the Switch save foulder:")
         self.switch_path_label.grid(row=7, column=0, padx=10, columnspan=2, sticky="W")
+
+        self.image_path_label = ctk.CTkLabel(master=self, text="Select the path to the Image of the game:")
+        self.image_path_label.grid(row=9, column=0, padx=10, columnspan=2, sticky="W")
+
+        self.wanring = ctk.CTkLabel(master=self, text="")
+        self.wanring.grid(row=12, column=0, columnspan=2, sticky="EW")
         # END Labels
 
         # ENTRY
         self.name_entry = ctk.CTkEntry(master=self, width=200)
-        self.name_entry.grid(row=1, column=0, 
-                        pady=10, padx=10, sticky="W", columnspan=3)
+        self.name_entry.grid(row=1, column=0, pady=10, padx=10, sticky="W", columnspan=3)
 
         self.pc_path_entry = ctk.CTkEntry(master=self)
-        self.pc_path_entry.grid(row=4, column=0, 
-                        pady=10, padx=10, sticky="W")
-
+        self.pc_path_entry.grid(row=4, column=0, pady=10, padx=10, sticky="W")
+        
         self.switch_path_entry = ctk.CTkEntry(master=self)
         self.switch_path_entry.grid(row=8, column=0, pady=10, padx=10, sticky="W")
+
+        self.image_path_entry = ctk.CTkEntry(master=self)
+        self.image_path_entry.grid(row=10, column=0, pady=10, padx=10, sticky="W")
         # END Entry
 
         # Buttons
-        self.pc_window_button = ctk.CTkButton(master=self, text="Janela", width=10, 
-                                    command=lambda: newsave_src.search_window("pc"))
+        self.pc_window_button = ctk.CTkButton(master=self, text="Window", width=10, 
+                                    command=lambda: new_save.search_window(self.pc_path_entry))
         self.pc_window_button.grid(row=4, column=1, sticky="W")
 
-        self.switch_window_button = ctk.CTkButton(master=self, text="Janela", width=10, 
-                                    command=lambda: newsave_src.search_window("switch"))
+        self.switch_window_button = ctk.CTkButton(master=self, text="Window", width=10, 
+                                    command=lambda: new_save.search_window(self.switch_path_entry))
         self.switch_window_button.grid(row=8, column=1, sticky="W")
 
-        self.send_button = ctk.CTkButton(master=self, command=lambda: newsave_src.send(), text="Concluir")
-        self.send_button.grid(row=9, pady=15, columnspan=2)
+        self.image_window_button = ctk.CTkButton(master=self, text="Window", width=10, 
+                                    command=lambda: new_save.search_window(self.image_path_entry))
+        self.image_window_button.grid(row=10, column=1, sticky="W")
+
+        self.send_button = ctk.CTkButton(master=self, command=lambda: new_save.send(), text="Concluir")
+        self.send_button.grid(row=11, pady=15, columnspan=2)
         # END Buttons
