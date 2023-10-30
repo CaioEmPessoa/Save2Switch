377714baf7ef52a5ebd53ac031805d9ad12f5b5a
CaioEmPessoa <caiopolonioabruno@gmail.com>
2023-10-30 18:33:33 -0300

    Changed config view and now saves selected optns

--- src/config.py
+++ src/config.py
@@ -1,6 +1,28 @@
 class Config():
     def create_config_window(self, main, config_view):
         self.main = main
-
         self.config_view = config_view.NewSaveView(self)
         self.config_view.grab_set()
+
+    def get_config_info(self):
+        self.switch_ip = self.config_view.switch_ip_entry.get()
+        self.save_app = self.config_view.save_app_select.get()
+
+        self.ftp_username = self.config_view.ftp_username_entry.get()
+        self.ftp_password = self.config_view.ftp_password_entry.get()
+
+        if self.switch_ip == "":
+            self.config_view.wanring.configure(fg_color="#b60000", text="Please put your switch IP.")
+            return
+        if self.ftp_username == "":
+            self.ftp_username = "anonymous"
+
+        self.config_data = {
+            "switch_ip": self.switch_ip,
+            "save_app": self.save_app,
+            "username": self.ftp_username,
+            "password": self.ftp_password
+        }
+
+        self.main.modify_data.write_data(self.config_data)
+        self.config_view.destroy()
--- views/config_view.py
+++ views/config_view.py
@@ -3,15 +3,52 @@ import customtkinter as ctk
 class NewSaveView(ctk.CTkToplevel):
     def __init__(self, new_save):
         super().__init__()
-        self.minsize(230, 260)
-        self.grid_rowconfigure((list(range(12))), weight=2)
-        self.grid_columnconfigure((list(range(3))), weight=2)
+        size = 420
 
-        switch_ip_label = ctk.CTkLabel(master=self, text="What is your switch's ip?")
-        switch_ip_label.grid(row=0, column=0)
+        # open screen centered
+        ws = self.winfo_screenwidth()
+        hs = self.winfo_screenheight()
+        x = (ws/2) - (size/2)
+        y = (hs/2) - (size/2)
+        self.geometry('%dx%d+%d+%d' % (size, size, x, y-80))
 
-        switch_ip_entry = ctk.CTkEntry(master=self)
-        switch_ip_entry.grid(row=1, column=0)
+        self.maxsize(size, size)
+        self.minsize(size, size)
 
-        ftp_app = ctk.CTkComboBox(master=self, values=["JKSV", "Edizon"])
-        ftp_app.grid(row=2, column=0)
+        self.grid_columnconfigure(0, weight=2)
+        self.grid_rowconfigure((list(range(7))), weight=2)
+        self.title("Save2Switch Config")
+
+        self.switch_ip_label = ctk.CTkLabel(master=self, text="What is your switch's ip?", font=('',16))
+        self.switch_ip_label.grid(row=0, column=0)
+
+        self.switch_ip_entry = ctk.CTkEntry(master=self, width=200, font=('',16))
+        self.switch_ip_entry.grid(row=1, column=0)
+
+        self.ftp_app_label = ctk.CTkLabel(master=self, text="What save app do you use on Switch?", font=('',16))
+        self.ftp_app_label.grid(row=2, column=0)
+
+        self.save_app_select = ctk.CTkOptionMenu(master=self, values=["JKSV", "Edizon"], width=200, font=('',16),
+                                        fg_color="White", text_color="Black", button_color=("#969da3", "#565a5f"))
+        self.save_app_select.grid(row=3, column=0)
+        
+        self.ftp_username_label = ctk.CTkLabel(master=self, text="Do you have a username for ftp?\n(recomend to leave it empty)", font=('',16))
+        self.ftp_username_label.grid(row=4, column=0)
+
+        self.ftp_username_entry = ctk.CTkEntry(master=self, width=200, font=('',16))
+        self.ftp_username_entry.grid(row=5, column=0)
+
+        self.ftp_password_label = ctk.CTkLabel(master=self, text="Do you have a password for ftp?\n(recomend to leave it empty)", font=('',16))
+        self.ftp_password_label.grid(row=6, column=0)
+
+        self.ftp_password_entry = ctk.CTkEntry(master=self, width=200, font=('',16))
+        self.ftp_password_entry.grid(row=7, column=0)
+
+        self.send_button = ctk.CTkButton(master=self, text="Confirm", font=('',16),
+                                    command=new_save.get_config_info)
+        self.send_button.grid(row=8, column=0, pady=15)
+
+        self.wanring = ctk.CTkLabel(master=self, text="")
+        self.wanring.grid(row=9, column=0, sticky="EW")
+
+        # send btn
