6987fc19fc304ee86f79d855634b20b0b919887d
CaioEmPessoa <caiopolonioabruno@gmail.com>
2023-11-01 20:01:56 -0300

    Trying to make a connection, still not working

--- src/ftp_connect.py
+++ src/ftp_connect.py
@@ -18,21 +18,33 @@ class connectFTP():
                 self.switch_connect.connect(HOSTNAME, PORT)
                 self.switch_connect.login(USERNAME, PASSWORD)
                 print(self.switch_connect.getwelcome())
+                print(self.switch_connect.nlst())
 
             except:
                 self.main.main_view.welcome.configure(text="Error connecting to FTP server.\nCheck your FTP info and if the server is opened.", 
                                                       text_color="Black")
+                
 
     def save2switch(self, game):
         self.connect()
         game = self.main.data["saves"][game]
-        src = game["pc_path"]
-        dst = game["switch_path"]
+        ip = self.main.data["switch_ip"]
+        port = self.main.data["switch_port"]
 
-        # Read file in binary mode
+        src = game["pc_path"]
+
+        if self.main.data["save_app"] == "JKSV":
+            dst = f"/JKSV/{game['switch_path']}/"
+        elif self.main.data["save_app"] == "EdiZon":
+            dst = f"/switch/EdiZon/saves/{game['switch_path']}/Save2Switch Copy/"
+
+        print(dst)
+        print(src)
+        self.switch_connect.cwd(dst)
         with open(src, "rb") as file:
-            # Command for Uploading the file "STOR filename"
-            self.switch_connect.storbinary(f"STOR {dst}", file)
+            self.switch_connect.storbinary(f"STOR vallsave.txt", file)
+        
+        self.switch_connect.quit()
 
     def save2pc(self, game):
         self.connect()
