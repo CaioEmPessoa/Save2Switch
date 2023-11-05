ebfd9cc44cc4b969be4aa50aa1b25feaa6370f2d
CaioEmPessoa <caiopolonioabruno@gmail.com>
2023-11-04 23:28:28 -0300

    Now can copy trees of save files to ftp switch

--- src/ftp_connect.py
+++ src/ftp_connect.py
@@ -10,7 +10,16 @@ class connectFTP():
         self.main = main
         self.today = datetime.datetime.now().strftime("%d.%m.%Y")
         
-    def connect(self):
+    def connect(self, game_name):
+        self.game = self.main.data["saves"][game_name]
+        self.pc_path = self.game["pc_path"]
+        match self.main.data["save_app"]:
+            case "JKSV":
+                "Save2Switch Copy - " + self.today
+                self.switch_path = f"/JKSV/{self.game['switch_path']}/"
+            case "EdiZon":
+                self.switch_path = f"/switch/EdiZon/saves/{self.game['switch_path']}/"
+
         if self.main.data["switch_ip"] != "0.0.0.0":
             try:
                 HOSTNAME = self.main.data["switch_ip"]
@@ -22,47 +31,57 @@ class connectFTP():
                 self.switch_connect.connect(HOSTNAME, PORT)
                 self.switch_connect.login(USERNAME, PASSWORD)
                 print(self.switch_connect.getwelcome())
-                print(self.switch_connect.nlst())
 
             except:
                 self.main.main_view.welcome.configure(text="Error connecting to FTP server.\nCheck your FTP info and if the server is opened.", 
                                                       text_color="Black")
+                raise
+
+    def copy_tree(self, src):
+        for file in os.listdir(src):
+            localpath = f"{src}/{file}"
+            print(self.switch_connect.nlst())
+            if os.path.isfile(localpath):
+                print("STOR", localpath, file)
+                self.switch_connect.storbinary('STOR ' + file, open(localpath,'rb'))
+            elif os.path.isdir(localpath):
+                print("MKD", file)
+                try:
+                    self.switch_connect.mkd(file)
+
+                # ignore "directory already exists"
+                except:
+                    pass
                 
+                print("CWD", file)
+                self.switch_connect.cwd(file)
+
+                self.copy_tree(localpath)
+                
+                print("CWD", "..")
+                self.switch_connect.cwd("..")
 
     def save2switch(self, game_name):
-        self.connect()
-        game = self.main.data["saves"][game_name]
-        src = game["pc_path"]
+        self.connect(game_name)
 
-        match self.main.data["save_app"]:
-            case "JKSV":
-                dst = f"\\JKSV\\{game['switch_path']}\\"
-            case "EdiZon":
-                dst = f"\\switch\\EdiZon\\saves\\{game['switch_path']}\\"
-
-        # change work directory
-        self.switch_connect.cwd(dst)
+        print('CWD ', self.switch_path)
+        self.switch_connect.cwd(self.switch_path)
+        print('CWD ', "OK!")
         
         new_dir_name = "Save2Switch Copy - " + self.today
-        print(new_dir_name)
+
         try:
+            print('MKD ', new_dir_name)
             self.switch_connect.mkd(new_dir_name)
-        except:
+            print('MKD ', "OK!")
+        except ftplib.error_perm:
+            print('MKD ', "OK!")
             pass
 
-        if os.path.isfile(src):
-            file_name = os.path.basename(src)
-            with open(src, "rb") as file:
-                self.switch_connect.storbinary(f"STOR {new_dir_name}\\{file_name}", file)
-        elif os.path.isdir(src):
-            save_files = os.listdir(src)
-            for file in save_files:
-                file_name = os.path.basename(file)
-                print(file_name)
-                with open(f"{src}\\{file}", "rb") as file:
-                    self.switch_connect.storbinary(f"STOR {new_dir_name}\\{file_name}", file)
+        print('CWD ', new_dir_name)
+        self.switch_connect.cwd(new_dir_name)
 
-        
+        self.copy_tree(self.pc_path)
         self.switch_connect.quit()
 
     def save2pc(self, game_name):
@@ -79,9 +98,6 @@ class connectFTP():
         for file in os.listdir(dst):
             shutil.copy(f"{dst}\\{file}", backhup_foulder)
 
-        match self.main.data["save_app"]:
-            case "JKSV":
-                src = f"\\JKSV\\{game['switch_path']}\\"
-            case "EdiZon":
-                src = f"\\switch\\EdiZon\\saves\\{game['switch_path']}\\"
+        save_files = self.switch_connect.nlst()
+
 
