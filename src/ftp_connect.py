e94d66c211390d843ce126e53e60a2a2dc5bcf46
CaioEmPessoa <caiopolonioabruno@gmail.com>
2023-11-08 20:29:26 -0300

    Save copying working for both sides as intended

--- src/ftp_connect.py
+++ src/ftp_connect.py
@@ -9,6 +9,7 @@ class connectFTP():
 
         self.main = main
         self.today = datetime.datetime.now().strftime("%d.%m.%Y")
+        self.today_precise = datetime.datetime.now().strftime("%d.%m.%Y - %H;%M;%S")
         
     def connect(self, game_name):
         self.game = self.main.data["saves"][game_name]
@@ -40,6 +41,39 @@ class connectFTP():
                 self.main.main_view.welcome.configure(text="Error connecting to FTP server.\nCheck your FTP info and if the server is opened.", 
                                                       text_color="Black")
                 raise
+
+    def download_tree(self, src, dst):       
+        print("CWD ", src)
+        self.switch_connect.cwd(src)
+
+        src_list = [os.path.basename(file) for file in self.switch_connect.nlst()]
+        dst_list = os.listdir(dst)
+        # try, if path not exist, create the src foulder|^|
+
+        for file in src_list:
+            try:
+                self.switch_connect.cwd(file)
+                self.switch_connect.cwd("..")
+                isfile = False
+            except:
+                isfile = True
+            
+
+            if isfile:
+                dst_file = os.path.join(dst, file)
+                if file in dst_list:
+                    os.remove(dst_file)
+
+                print("RETR ", file)
+                self.switch_connect.retrbinary("RETR " + file, open(dst_file, 'wb').write)
+
+            elif not isfile:
+                print("LOCAL: MKDIR", file)
+                self.download_tree(file, os.path.join(dst, file))
+                self.switch_connect.cwd("..")
+            
+            else:
+                print("???")
 
     def upload_tree(self, src):
         for file in os.listdir(src):
@@ -78,6 +112,10 @@ class connectFTP():
             self.switch_connect.mkd(new_dir_name)
             print('MKD ', "OK!")
         except ftplib.error_perm:
+            self.today_precise = datetime.datetime.now().strftime("%d.%m.%Y - %H;%M;%S")
+            new_dir_name = "Save2Switch Copy - " + self.today_precise
+            print('MKD ', new_dir_name)
+            self.switch_connect.mkd(new_dir_name)
             print('MKD ', "OK!")
             pass
 
@@ -89,38 +127,26 @@ class connectFTP():
 
     def save2pc(self, game_name):
         self.connect(game_name)
-        src = os.path.join(self.switch_path, self.game["switch_foulder"])
+        src = self.game["switch_foulder"]
         dst = self.game["pc_path"]
 
-        # PC SAVE BACKUP <---------------------------------------------------------------------
+        # PC SAVE BACKUP <-----------------------------------------------------
         backhup_foulder = f"{os.getcwd()}\\backups\\{self.game['name']} backup - {self.today}"
         
         try:
             os.mkdir(backhup_foulder)
         except FileExistsError:
-            pass
+            self.today_precise = datetime.datetime.now().strftime("%d.%m.%Y - %H;%M;%S")
+            backhup_foulder = f"{os.getcwd()}\\backups\\{self.game['name']} backup - {self.today_precise}"
+            os.mkdir(backhup_foulder)
 
-        try:
-            print("Backing up pc save foulder...")
-            shutil.copytree(dst, backhup_foulder)
-        except FileExistsError:
-            print("Save foulder with backup already")
-            pass
+        for file in os.listdir(dst):
+            file_path = f"{dst}/{file}"
+            if os.path.isfile(file_path):
+                shutil.copy(file_path, backhup_foulder)
+            elif os.path.isdir(file_path):
+                shutil.copytree(file_path, f"{backhup_foulder}\\{file}")
+
         # >------------------------------------------------------------------------> END BACKUP
 
-        self.switch_connect.cwd(src)
-
-        src_list = [os.path.basename(file) for file in self.switch_connect.nlst()]
-        dst_list = os.listdir(dst)
-
-        print(self.switch_connect.nlst())
-
-        for file in src_list:
-            dst_file = os.path.join(dst, file)
-            if file in dst_list:
-                os.remove(dst_file)
-
-            print("RETR ", dst_file)
-            self.switch_connect.retrbinary("RETR " + file, open(dst_file, 'wb').write)
-
-
+        self.download_tree(src, dst)
